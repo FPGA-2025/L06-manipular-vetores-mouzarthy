ebf04f4fd9fd35dc56f5387d9774df7544e59574
mouzarthy <mouzarthy2007@hotmail.com>
2025-04-15 16:25:02 -0300

    Tarefa-manipular vetores

--- manipular_vetores.v
+++ manipular_vetores.v
@@ -2,6 +2,16 @@ module manipular_vetores(
     input [31:0] entrada,
     output [31:0] saida );
 
-    // insira seu código aqui
+    wire [3:0] b1, b2, b3, b4, b5, b6, b7, b8;
 
+    assign b1 = entrada[31:28];
+    assign b2 = entrada[27:24];
+    assign b3 = entrada[23:20];
+    assign b4 = entrada[19:16];
+    assign b5 = entrada[15:12];
+    assign b6 = entrada[11:8];
+    assign b7 = entrada[7:4];
+    assign b8 = entrada[3:0];
+
+    assign saida ={ b7, b8, b5, b6, b3, b4, b1, b2 };
 endmodule
